757cdb566b42bb0eeba2c9aaad7deecd84e8c200
kestsaurav21 <kestsaurav21@outlook.com>
2024-11-22 00:54:10 +0530

    setup routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import BuyCredit from './pages/BuyCredit'
+import Result from './pages/Result'
 
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <>
+    <Router>
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/buy' element={<BuyCredit />} />
+            <Route path='/buy' element={<Result />} />
+        </Routes>
+    </Router>
+    </>
   )
 }
 
