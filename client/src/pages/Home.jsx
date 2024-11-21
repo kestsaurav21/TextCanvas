da4cfc61e3afc361167527abfa47e343d813b88a
kestsaurav21 <kestsaurav21@outlook.com>
2024-11-22 05:17:45 +0530

    update: home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
+import Header from '../components/Header'
+import { assets } from '../assets/assets'
+import Steps from '../components/Steps'
+import Description from '../components/Description'
+import Testimonials from '../components/Testimonials'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+    <Header />
+    <Steps />
+    <Description />
+    <Testimonials />
+
+    </div>
   )
 }
 
