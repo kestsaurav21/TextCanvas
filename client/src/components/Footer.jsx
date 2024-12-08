84900f9d55660728f29fdcfb64d1294f479173cb
kestsaurav21 <kestsaurav21@outlook.com>
2024-12-09 02:46:35 +0530

    fix: footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,8 +5,8 @@ import { assets } from '../assets/assets';
 
 const Footer = () => {
   return (
-    <footer className="w-full px-4 py-8 bg-black flex items-center justify-evenly">
-      <div className="flex flex-col items-center gap-4 md:flex-row md:gap-8">
+    <footer className="w-full px-4 py-8 bg-black flex flex-col items-center justify-evenly">
+      <div className="flex flex-col items-center gap-4 md:flex-row md:gap-8 mb-4">
         
         <p className="text-white text-sm font-bold text-center md:text-left md:text-base lg:text-xl">
           All rights reserved. © 2024 TextCanvas
